2a3748251cda39490352f33622a2055019629310
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-03-15 22:34:06 +0530

    feat: core/ and models/ for the projecct

---
+++ apps/api/app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    APP_NAME: str = "Personal Knowledge Memory API"
+    OPENAI_API_KEY: SecretStr
+    MODEL_NAME: str = "meta-llama/llama-3.1-8b-instruct"
+    DATABASE_URL: str
+
+
+settings = Settings()
