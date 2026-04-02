79224640f702a66ef175223b09214b9c08792312
Shajeeve <Shajeeve567@users.noreply.github.com>
2026-04-02 22:10:24 +0530

    fix: project structure correction with some minor code changes

--- apps/api/app/core/config.py
+++ apps/api/app/core/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
-
+from pathlib import Path
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=str(Path(__file__).parent.parent.parent.parent.parent / ".env"),
         env_file_encoding="utf-8",
         extra="ignore",
     )
